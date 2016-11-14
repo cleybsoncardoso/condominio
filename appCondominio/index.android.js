7548556070da7dcf002a5e207a5d5ab99fda08e5
cleybson cardoso <cleybsonc@gmail.com>
2016-11-13 21:53:14 -0300

    testes

--- appCondominio/index.android.js
+++ appCondominio/index.android.js
@@ -1,23 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-class Teste extends Component {
+export default class Teste extends Component {
   render() {
     return (
-      <Text>teste!</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Testa ai
+        </Text>
+      </View>
     );
   }
 }
 
-class HelloWorldApp extends Component {
+export default class appCondominio extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
-      <Teste/>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+        <Teste/>
+      </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
-
-AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp);
+AppRegistry.registerComponent('appCondominio', () => appCondominio);
